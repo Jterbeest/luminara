8e7435ed93f267514c3aa7688d50f2966ef91b56
Johan ter Beest <jterbeest@ripe.net>
2016-10-23 10:42:52 +0200

    admin for the peering db tables

--- peeringdb/admin.py
+++ peeringdb/admin.py
@@ -1,3 +1,85 @@
 from django.contrib import admin
 
-# Register your models here.
+import django_peeringdb.models
+from .models import (
+  Organization,
+  Network,
+  InternetExchange,
+  Facility,
+  NetworkContact,
+  NetworkIXLan,
+  NetworkFacility,
+  IXLan,
+  IXLanPrefix,
+  InternetExchangeFacility
+)
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(Organization, OrganizationAdmin)
+
+
+class NetworkAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(Network, NetworkAdmin)
+
+
+class InternetExchangeAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(InternetExchange, InternetExchangeAdmin)
+
+
+class FacilityAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(Facility, FacilityAdmin)
+
+
+class NetworkContactAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(NetworkContact, NetworkContactAdmin)
+
+
+class NetworkIXLanAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(NetworkIXLan, NetworkIXLanAdmin)
+
+
+class NetworkFacilityAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(NetworkFacility, NetworkFacilityAdmin)
+
+
+class IXLanAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(IXLan, IXLanAdmin)
+
+
+class IXLanPrefixAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(IXLanPrefix, IXLanPrefixAdmin)
+
+
+class InternetExchangeFacilityAdmin(admin.ModelAdmin):
+
+    pass
+
+admin.site.register(InternetExchangeFacility, InternetExchangeFacilityAdmin)
